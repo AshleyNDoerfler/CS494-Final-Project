efd3069fdc19ab13b66550f1e0ba35dd8b3a3a80
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2025-06-10 22:43:38 -0700

    Make changes for vercel

--- src/api/letters/route.ts
+++ src/api/letters/route.ts
@@ -13,7 +13,7 @@ export async function POST(req: NextRequest) {
         setLetterInCache(letter);
 
         return NextResponse.json({ success: true, letter });
-    } catch (_) {
+    } catch {
         return NextResponse.json({ error: "Failed to set letter in cache" }, { status: 500 });
     }
 }
