9abf973841c8ea1261d1dcc9522b1b0e704a891c
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2025-06-10 23:05:15 -0700

    See if this one will display on vercel

--- src/app/gameover/page.tsx
+++ src/app/gameover/page.tsx
@@ -1,16 +1,17 @@
 'use client';
 
-import { Box } from '@mui/material'
+import { Box, CardMedia } from '@mui/material'
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <Box>
         <h2>loser :p</h2>
-        <Image
-            src={'/lose.jpg'}
+        
+        <CardMedia
+            component="img"
+            image={'/lose.jpg'}
             alt={"This cat will eat ou cause you lost"}
-            width={500}
             height={500}
         />
     </Box>
